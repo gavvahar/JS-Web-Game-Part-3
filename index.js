eb775977a7d7df527c6d8b48e6fafe94c506dd68
gavvahar <nihargavva@gmail.com>
2022-06-21 12:07:54 -0400

    forgot a step in the second part

--- index.js
+++ index.js
@@ -11,21 +11,14 @@ function move(image) {
   };
 }
 
-let greenCharacter = newImage("assets/green-character.gif");
-let thingThatMoveReturns = move(greenCharacter);
-thingThatMoveReturns.to(100, 250);
-
-function newImage(url, left, bottom) {
+function newImage(url) {
   let image = document.createElement("img");
   image.src = url;
-  image.style.position = "fixed";
-  image.style.left = left + "px";
-  image.style.bottom = bottom + "px";
   document.body.append(image);
   return image;
 }
 
-// newImage("assets/green-character.gif", 100, 250);
+move(newImage("assets/green-character.gif")).to(100, 250);
 newImage("assets/tree.png", 200, 450);
 newImage("assets/pillar.png", 350, 250);
 newImage("assets/pine-tree.png", 450, 350);
